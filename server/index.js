5492f94abb73e74e3919cce1c2b3f5b358878bdd
Muhammad Awais Shah <82607059+awaisaslam186@users.noreply.github.com>
2021-08-15 13:58:19 +0500

    register end points

--- server/index.js
+++ server/index.js
@@ -1,15 +1,27 @@
 // importing packages
 const express=require('express')
+const mongoose=require('mongoose')
+const dotenv=require('dotenv').config()
 
 //initialization
 const app=express()
 const PORT=process.env.PORT || 5000
 
+// DB connection
+mongoose.connect(process.env.MDB_CONNECT,{useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+    console.log("Database Connected")
+})
+//  middleware
+app.use(express.json())
+app.use('/auth',require('./router/userRouter'))
 //End Points
 app.get('/',(req,res)=>{
     res.send(`Hello world`)
 })
+
 // running server
 app.listen(PORT,()=>{
     console.log(`Hi Server started on Port ${PORT}`)
 })
+//proj=0: mongodb+srv://awaisshah228:Daanish#2280@cluster0.icksj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+//proj=1: 
---
+++ server/router/userRouter.js
@@ -0,0 +1,35 @@
+const router=require('express').Router()
+const user=require('../model/userModel')
+router.post('/',async (req,res)=>{
+    try{
+        const {email,password,passverify}=req.body
+        //validation
+        if(!email|| !password || !passverify){
+            return res.status(400).json({error:"Enter Missing Field"})
+        }
+       
+        if(password.length<6){
+            
+            return res.status(400).json({error :"Password lenght must be greater than 6 chracter"})
+        }
+        if(password!=passverify){
+            return res.status(400).json({error :"Enter verify pass"})
+
+        }
+        const existingUser=await user.findOne({email})
+        if(existingUser){
+            return res.status(400).json({
+                error: "Email Already register"
+            })
+        }
+
+        console.log(email)
+        res.send("Test")
+    }
+    catch(err){
+        console.error(err)
+        res.status(500).send()
+    }
+    
+})
+module.exports=router
